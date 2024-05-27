888e349e71b52a75b4402042a54ef5775529a09b
Rai Siqueira <rai93siqueira@gmail.com>
2024-05-27 19:09:53 -0300

    fix bettervim settings
    Signed-off-by: Rai Siqueira <rai93siqueira@gmail.com>

--- .config/nvim/lua/better-vim-core/constants.lua
+++ .config/nvim/lua/better-vim-core/constants.lua
@@ -2,7 +2,7 @@ local home = os.getenv "HOME"
 
 local M = {
   POSTHOG_API_KEY = "phc_PVuiistyDKKOX1sV6K1SJ84t001N1zPItEGFUKhlxvn",
-  CONFIG_PATH = home .. "/.config/nvim/lua/better-vim",
+  CONFIG_PATH = home .. "/.config/better-vim",
 }
 
 return M
