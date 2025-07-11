f668f14f57126b02b4146d0d4472d91ee731981f
Rai Siqueira <rai93siqueira@gmail.com>
2025-07-10 21:06:39 -0300

    chore: update setup for Debian environments
    Signed-off-by: Rai Siqueira <rai93siqueira@gmail.com>

--- setup.sh
+++ setup.sh
@@ -11,20 +11,34 @@ git config --global user.email "$git_config_email"
 echo "Update the system"
 cd ~ && sudo apt update && sudo apt upgrade -y
 
+echo "Install Homebrew"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 echo "Install the basic packages"
 
 sudo apt install -y \
-    build-essential \
-    curl \
-    software-properties-common \
-    unzip \
-    wget \
-    zip \
-    stow \
-    zsh \
-    tmux \
-    fzf \
-    zoxide
+  build-essential \
+  curl \
+  software-properties-common \
+  unzip \
+  wget \
+  zip \
+  stow \
+  zsh \
+  tmux \
+  fzf \
+  zoxide
+
+echo "Installing brew packages"
+
+brew "bat"
+brew "eza"
+brew "fd"
+brew "lazygit"
+brew "tree-sitter"
+brew "neovim"
+brew "ripgrep"
 
 echo "Set zsh as the default shell"
 chs -s $(which zsh)
@@ -35,12 +49,8 @@ stow --adopt .
 
 source ~/.zshrc
 
-# echo 'Installing NodeJS LTS'
-# nvm --version
-# nvm install --lts
-# nvm current
-
 echo "Bumping the max file watchers"
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
 
 echo "All setup, enjoy!"
+
